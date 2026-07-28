f6da8fdeda95214a0f943c01422b0671aaacad4a
Jebuxs <villjebuxs@gmail.com>
2026-07-28 13:08:25 -0500

    Create firebase-config.js

---
+++ core/firebase-config.js
@@ -0,0 +1,11 @@
+// ============================================================
+// CONFIGURACIÓN DE FIREBASE (CORE)
+// ============================================================
+export const firebaseConfig = {
+    apiKey: "AIzaSyB2p4mFewLlKojEAAfMfNi76U9K3HiIiAo",
+    authDomain: "ventas-dcc22.firebaseapp.com",
+    projectId: "ventas-dcc22",
+    storageBucket: "ventas-dcc22.firebasestorage.app",
+    messagingSenderId: "789904237680",
+    appId: "1:789904237680:web:45b32dd8e211193e1e40b5"
+};
