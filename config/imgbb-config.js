3cc03d150c72a37f7b600bf707de7d799820aaad
Jebuxs <villjebuxs@gmail.com>
2026-07-29 05:15:25 -0500

    Create imgbb-config.js

---
+++ config/imgbb-config.js
@@ -0,0 +1,5 @@
+// ============================================================
+// CONFIGURACIÓN DE IMGBB (API KEY)
+// ============================================================
+export const IMGBB_API_KEY = "cb12e6a76abb14df50fa90b78479a43c";
+export const IMGBB_ENDPOINT = "https://api.imgbb.com/1/upload";
