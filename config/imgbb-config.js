57466882d036caa5de46379412139401f8e54636
Jebuxs <villjebuxs@gmail.com>
2026-07-30 05:35:44 -0500

    Update imgbb-config.js

--- config/imgbb-config.js
+++ config/imgbb-config.js
@@ -1,5 +1,5 @@
 // ============================================================
-// CONFIGURACIÓN DE IMGBB (API KEY)
+// CONFIGURACIÓN DE IMGBB
 // ============================================================
 export const IMGBB_API_KEY = "cb12e6a76abb14df50fa90b78479a43c";
-export const IMGBB_ENDPOINT = "https://api.imgbb.com/1/upload";
+export const IMGBB_API_URL = "https://api.imgbb.com/1/upload";
